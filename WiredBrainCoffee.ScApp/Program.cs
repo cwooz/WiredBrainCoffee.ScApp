cb880017ffe044d1aedd0322375d8411cdd79389
Chris Woosley <woosleyc@vystarcu.org>
2021-10-27 16:19:28 -0400

    update

--- WiredBrainCoffee.ScApp/Program.cs
+++ WiredBrainCoffee.ScApp/Program.cs
@@ -6,7 +6,34 @@ namespace WiredBrainCoffee.ScApp
     {
         static void Main(string[] args)
         {
-            
+            _ = new Container<string>();
+            _ = new Container<string>();
+            _ = new Container<int>();
+
+            Console.WriteLine($"Container<string>: {Container<string>.InstanceCount}");
+            Console.WriteLine($"Container<int>: {Container<int>.InstanceCount}");
+            Console.WriteLine($"Container<bool>: {Container<bool>.InstanceCount}");
+            Console.WriteLine($"ContainerBase>: {ContainerBase.InstanceCountBase}");
         }
+    }
+
+    class ContainerBase
+    {
+        public ContainerBase()
+        {
+            InstanceCountBase++;
+        }
+
+        public static int InstanceCountBase { get; private set; }
+    }
+    
+    class Container<T> : ContainerBase
+    {
+        public Container()
+        {
+            InstanceCount++;
+        }
+
+        public static int InstanceCount { get; private set; }
     }
 }
