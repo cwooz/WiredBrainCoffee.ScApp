36989c86e62bbf196a9e96b858a936dea59750ba
Chris Woosley <woosleyc@vystarcu.org>
2021-10-28 12:02:18 -0400

    update

--- WiredBrainCoffee.ScApp/Program.cs
+++ WiredBrainCoffee.ScApp/Program.cs
@@ -8,12 +8,15 @@ namespace WiredBrainCoffee.ScApp
         {
             _ = new Container<string>();
             _ = new Container<string>();
-            _ = new Container<int>();
+            //_ = new Container<int>();
+            var container = new Container<int>();
 
             Console.WriteLine($"Container<string>: {Container<string>.InstanceCount}");
             Console.WriteLine($"Container<int>: {Container<int>.InstanceCount}");
             Console.WriteLine($"Container<bool>: {Container<bool>.InstanceCount}");
             Console.WriteLine($"ContainerBase>: {ContainerBase.InstanceCountBase}");
+
+            container.PrintItem<string>("From generic method in generic class");
         }
     }
 
@@ -35,5 +38,10 @@ namespace WiredBrainCoffee.ScApp
         }
 
         public static int InstanceCount { get; private set; }
+
+        public void PrintItem<TItem>(TItem item)
+        {
+            Console.WriteLine($"Item: {item}");
+        }
     }
 }
